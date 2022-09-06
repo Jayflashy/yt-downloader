87d9ee5dfe8c21f4b10e299ccc8524a22a03609a
jayflashy <jadesdevelopers@gmail.com>
2022-09-06 11:06:35 +0100

    donload video oops

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 const  express = require('express')
 const router = express.Router()
+const ytdl = require('ytdl-core')
 
 router.get('/', (req, res) => {
     res.render('index', {title: "Youtube Downloader"})
 })
 router.post('/download', (req, res) => {
-    console.log('Okauiiiai b')
+    // get video link
+    const vlink = req.body.link
+    console.log(vlink)
+    // perform donload oop
     res.send('Okay dhjl')
 })
 module.exports = router
